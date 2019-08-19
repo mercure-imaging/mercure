bf9880807f76d6efdf15964eb32951918bae73f1
tblock79 <t.block@seginus.de>
2019-08-19 18:32:48 +0200

    Added bookkeeper address to receiver.

--- receiver.sh
+++ receiver.sh
@@ -3,5 +3,4 @@ incoming=/hermes-data/incoming
 binary="$HOME/hermes/bin/getdcmtags"
 
 echo "Starting DICOM receiver..."
-storescp --fork --promiscuous -od "$incoming" +uf -xcr "$binary $incoming/#f" 104
-
+storescp --fork --promiscuous -od "$incoming" +uf -xcr "$binary $incoming/#f 0.0.0.0:8080" 104
