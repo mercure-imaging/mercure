6a12fb5dc15ade8e8792325a70213249a2c6f409
tblock79 <t.block@seginus.de>
2019-08-20 17:35:21 +0200

    Fixed script for receiver.

--- receiver.sh
+++ receiver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-incoming=/hermes-data/incoming
-binary="$HOME/hermes/bin/getdcmtags"
+incoming=/home/hermes/hermes-data/incoming
+binary=/home/hermes/hermes/bin/getdcmtags
 
 echo "Starting DICOM receiver..."
 storescp --fork --promiscuous -od "$incoming" +uf -xcr "$binary $incoming/#f 0.0.0.0:8080" 104
