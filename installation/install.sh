e08002cd501398f0f8b158ba574c97654ac27e0b
evanoost <evanoost@ur.rochester.edu>
2021-04-15 10:58:15 -0400

    This makes it a little bit easier to rebase Mercure to a different directory.
    Also fixed a type in configuration that caused the script to fail

--- installation/install.sh
+++ installation/install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+# Make defaults easily overrideable
+MERCUREBASE=$HOME
+
 echo "Installing Python runtime environment..."
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME/miniconda.sh"
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$MERCUREBASE/miniconda.sh"
 bash ~/miniconda.sh -b -p ~/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda create -y -q --prefix "$HOME/mercure-env" python=3.6
+export PATH="$MERCUREBASE/miniconda/bin:$PATH"
+conda create -y -q --prefix "$MERCUREBASE/mercure-env" python=3.6
 
 echo "Installing required Python packages..."
-$HOME/mercure-env/bin/pip install --quiet -r "$HOME/mercure/requirements.txt"
+$MERCUREBASE/mercure-env/bin/pip install --quiet -r "$MERCUREBASE/mercure/requirements.txt"
 
 echo "Creating default configuration files..."
-cp ../configuration/default_bookkeeper.env ../configuration/bookkeeper.env 
-cp ../configuration/default_mercure.json ../configuration/mercure.json
-cp ../configuration/default_services.json ../configuration/services.json
-cp ../configuration/default_webgui.env ../configuration/webgui.env 
+cp $MERCUREBASE/mercure/configuration/default_bookkeeper.env $MERCUREBASE/mercure/configuration/bookkeeper.env
+cp $MERCUREBASE/mercure/configuration/default_mercure.json $MERCUREBASE/mercure/configuration/mercure.json
+cp $MERCUREBASE/mercure/configuration/default_services.json $MERCUREBASE/mercure/configuration/services.json
+cp $MERCUREBASE/mercure/configuration/default_webgui.env $MERCUREBASE/mercure/configuration/webgui.env
 
 echo "Done. Please continue installation according to mercure user guide."
 echo ""
