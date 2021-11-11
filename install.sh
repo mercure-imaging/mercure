f70b977c1a8baf17fa9f6bcc79cd0a3f90d26604
Roy Wiggins <roywig@gmail.com>
2021-11-11 22:45:42 +0000

    update install script; first pass at systemd install

--- install.sh
+++ install.sh
@@ -27,70 +27,163 @@ if [ "$DB_PWD" = "unset" ]
 then
   DB_PWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 || true)
 fi
-
 MERCURE_BASE=/opt/mercure
 DATA_PATH=$MERCURE_BASE/data
 CONFIG_PATH=$MERCURE_BASE/config
 DB_PATH=$MERCURE_BASE/db
 MERCURE_SRC=.
 
-if [[ ! -e $MERCURE_BASE ]]; then
-    echo "Creating $MERCURE_BASE..."
-    sudo mkdir -p $MERCURE_BASE
-    sudo chown $OWNER:$OWNER $MERCURE_BASE
-    sudo chmod a+x $MERCURE_BASE
+if [ -f "$CONFIG_PATH"/db.env ]; then 
+  source "$CONFIG_PATH"/db.env # Don't accidentally generate a new database password
+  DB_PWD=$POSTGRES_PASSWORD
 fi
 
-if [[ ! -e $DATA_PATH ]]; then
-    echo "Creating $DATA_PATH..."
-    sudo mkdir "$DATA_PATH"
-    sudo mkdir "$DATA_PATH"/incoming "$DATA_PATH"/studies "$DATA_PATH"/outgoing "$DATA_PATH"/success
-    sudo mkdir "$DATA_PATH"/error "$DATA_PATH"/discard "$DATA_PATH"/processing
-    sudo chown -R $OWNER:$OWNER $DATA_PATH
-    sudo chmod a+x $DATA_PATH
+
+echo "Mercure installation folder: $MERCURE_BASE"
+echo "Data folder: $DATA_PATH"
+echo "Config folder: $CONFIG_PATH"
+echo "Database folder: $DB_PATH"
+echo "Mercure source directory: $(readlink -f $MERCURE_SRC)"
+
+create_folder () {
+  if [[ ! -e $1 ]]; then
+    echo "Creating $1"
+    sudo mkdir -p $1
+    sudo chown $OWNER:$OWNER $1
+    sudo chmod a+x $1
+  else
+    echo "$1 already exists."
+  fi
+}
+create_folders () {
+  create_folder $MERCURE_BASE
+  create_folder $CONFIG_PATH
+  if [ $INSTALL_TYPE = "docker" ]; then
+    create_folder $DB_PATH
+  fi
+
+  if [[ ! -e $DATA_PATH ]]; then
+      echo "Creating $DATA_PATH..."
+      sudo mkdir "$DATA_PATH"
+      sudo mkdir "$DATA_PATH"/incoming "$DATA_PATH"/studies "$DATA_PATH"/outgoing "$DATA_PATH"/success
+      sudo mkdir "$DATA_PATH"/error "$DATA_PATH"/discard "$DATA_PATH"/processing
+      sudo chown -R $OWNER:$OWNER $DATA_PATH
+      sudo chmod a+x $DATA_PATH
+  else
+    echo "$DATA_PATH already exists."
+  fi
+}
+
+install_configuration () {
+  if [ ! -f "$CONFIG_PATH"/mercure.json ]; then
+    echo "Copying configuration files..."
+    cp "$MERCURE_SRC"/configuration/default_bookkeeper.env "$CONFIG_PATH"/bookkeeper.env
+    cp "$MERCURE_SRC"/configuration/default_mercure.json "$CONFIG_PATH"/mercure.json
+    cp "$MERCURE_SRC"/configuration/default_services.json "$CONFIG_PATH"/services.json
+    cp "$MERCURE_SRC"/configuration/default_webgui.env "$CONFIG_PATH"/webgui.env
+    echo "POSTGRES_PASSWORD=$DB_PWD" > "$CONFIG_PATH"/db.env
+
+    if [ $INSTALL_TYPE = "systemd" ]; then 
+      sed -i -e "s/mercure:ChangePasswordHere@localhost/mercure:$DB_PWD@localhost/" "$CONFIG_PATH"/bookkeeper.env
+    elif [ $INSTALL_TYPE = "docker" ]; then
+      sed -i -e "s/mercure:ChangePasswordHere@localhost/mercure:$DB_PWD@db/" "$CONFIG_PATH"/bookkeeper.env
+      sed -i -e "s/0.0.0.0:8080/bookkeeper:8080/" "$CONFIG_PATH"/mercure.json
+    fi
+    sed -i -e "s/PutSomethingRandomHere/$SECRET/" "$CONFIG_PATH"/webgui.env
+    sudo chown -R $OWNER:$OWNER "$CONFIG_PATH"
+    # sudo chmod -R a+r "$CONFIG_PATH"
+  fi
+}
+
+install_docker () {
+  if [ ! -f "$MERCURE_BASE"/docker-compose.yml ]; then
+    echo "Copying docker-compose.yml..."
+    cp $MERCURE_SRC/docker/docker-compose.yml $MERCURE_BASE
+    sudo chown $OWNER:$OWNER "$MERCURE_BASE"/docker-compose.yml
+  fi
+}
+
+install_docker_dev () {
+  if [ ! -f "$MERCURE_BASE"/docker-compose.override.yml ]; then
+    echo "Copying docker-compose.override.yml..."
+    cp $MERCURE_SRC/docker/docker-compose.override.yml $MERCURE_BASE
+    sed -i -e "s;MERCURE_SRC;$(readlink -f $MERCURE_SRC);" "$MERCURE_BASE"/docker-compose.override.yml
+    sudo chown $OWNER:$OWNER "$MERCURE_BASE"/docker-compose.override.yml
+  fi
+}
+
+install_conda() {
+  if [ ! -x "$(command -v conda)" ]; then # Can't find conda in PATH
+    if [ -e "/opt/miniconda" ]; then
+      PATH="/opt/miniconda/bin:$PATH"
+    else 
+      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "/tmp/miniconda.sh"
+      sudo bash /tmp/miniconda.sh -b -p /opt/miniconda
+    fi 
+  fi
+}
+
+
+install_app_files() {
+  if [ ! -e "$MERCURE_BASE"/app ]; then
+    echo "Installing app files..."
+    sudo mkdir "$MERCURE_BASE"/app
+    sudo find "$MERCURE_SRC" -not -path \*/.\* -type d -exec mkdir -p -- "$MERCURE_BASE"/app/{} \;
+    sudo find "$MERCURE_SRC" -not -path \*/.\* -type f -exec cp -- {} "$MERCURE_BASE"/app/{} \;
+  fi
+}
+
+install_packages() {
+  sudo apt-get install -y build-essential wget git dcmtk jq inetutils-ping sshpass install postgresql postgresql-contrib
+}
+
+install_dependencies() {
+  echo "Installing Python runtime environment..."
+  install_conda
+  if [ ! -e "$MERCURE_BASE/env" ]; then
+    conda create -y -q --prefix "$MERCURE_BASE/env" python=3.6
+    echo "Installing required Python packages..."
+    $MERCURE_BASE/env/bin/pip install --quiet -r "$MERCURE_BASE/app/requirements.txt"
+  fi
+}
+
+install_postgres() {
+  echo "Setting up postgres..."
+  sudo -u postgres -s <<- EOM
+    createuser mercure
+    createdb mercure -O mercure
+    psql -c "ALTER USER mercure WITH PASSWORD '$DB_PWD';"
+EOM
+}
+
+install_services() {
+  echo "Installing services..."
+  sudo cp "$MERCURE_SRC"/installation/*.service /etc/systemd/system
+  sudo systemctl enable mercure_bookkeeper.service mercure_cleaner.service mercure_dispatcher.service mercure_receiver.service mercure_router.service mercure_ui.service
+}
+
+systemd_install () {
+  create_folders
+  install_configuration
+  install_packages
+  install_app_files
+  install_dependencies
+  install_postgres
+  install_services
+}
+
+docker_install () {
+  create_folders
+  install_configuration
+  install_docker
+  install_docker_dev
+}
+
+INSTALL_TYPE="${1:-docker}"
+
+if [ $INSTALL_TYPE = "systemd" ]; then 
+  systemd_install
+elif [ $INSTALL_TYPE = "docker" ]; then
+  docker_install
 fi
-
-if [[ ! -e $CONFIG_PATH ]]; then
-    echo "Creating $CONFIG_PATH..."
-    sudo mkdir $CONFIG_PATH
-    sudo chown $OWNER:$OWNER $CONFIG_PATH
-    sudo chmod a+x $CONFIG_PATH
-fi
-
-if [[ ! -e $DB_PATH ]]; then
-    echo "Creating $DB_PATH..."
-    sudo mkdir $DB_PATH
-    sudo chown $OWNER:$OWNER $DB_PATH
-    sudo chmod a+x $DB_PATH
-fi
-
-if [ ! -f "$CONFIG_PATH"/mercure.json ]; then
-  echo "Copying configuration files..."
-  cp $MERCURE_SRC/configuration/default_bookkeeper.env "$CONFIG_PATH"/bookkeeper.env
-  cp $MERCURE_SRC/configuration/default_mercure.json "$CONFIG_PATH"/mercure.json
-  cp $MERCURE_SRC/configuration/default_services.json "$CONFIG_PATH"/services.json
-  cp $MERCURE_SRC/configuration/default_webgui.env "$CONFIG_PATH"/webgui.env
-  echo "POSTGRES_PASSWORD=$DB_PWD" > "$CONFIG_PATH"/db.env
-
-  # Change the PostgreSQL and mercure bookkeeper string to match your build (check docker-compose.yml)
-  sed -i -e "s/mercure:ChangePasswordHere@localhost/mercure:$DB_PWD@db/" "$CONFIG_PATH"/bookkeeper.env
-  sed -i -e "s/0.0.0.0:8080/bookkeeper:8080/" "$CONFIG_PATH"/mercure.json
-  sed -i -e "s/PutSomethingRandomHere/$SECRET/" "$CONFIG_PATH"/webgui.env
-  sudo chown -R $OWNER:$OWNER "$CONFIG_PATH"
-  sudo chmod -R a+r "$CONFIG_PATH"
-fi
-
-if [ ! -f "$MERCURE_BASE"/docker-compose.yml ]; then
-  echo "Copying docker-compose.yml..."
-  cp $MERCURE_SRC/docker/docker-compose.yml $MERCURE_BASE
-  sudo chown $OWNER:$OWNER "$MERCURE_BASE"/docker-compose.yml
-fi
-
-if [ ! -f "$MERCURE_BASE"/docker-compose.override.yml ]; then
-  echo "Copying docker-compose.override.yml..."
-  cp $MERCURE_SRC/docker/docker-compose.override.yml $MERCURE_BASE
-  sed -i -e "s;MERCURE_SRC;$(readlink -f $MERCURE_SRC);" "$MERCURE_BASE"/docker-compose.override.yml
-  sudo chown $OWNER:$OWNER "$MERCURE_BASE"/docker-compose.override.yml
-fi
-
 echo "Installation complete"
