e99a986733511acc7c4f9cb245ca2481e8b268c2
Tobias Block <tobias.block@nyumc.org>
2022-02-04 07:43:20 +0000

    Updated docker build scripts

--- build-docker.sh
+++ build-docker.sh
@@ -18,7 +18,17 @@
 # Change prefix to your own Docker prefix if you want to make 
 # your own custom build
 PREFIX="mercureimaging"
-TAG=${MERCURE_TAG:-latest}
+
+# Read the version of the mercure source, which will be used for
+# tagging the Docker images, unless a tag has been provided 
+# through the environment variable MERCURE_TAG
+if [ ! -f "VERSION" ]; then
+    echo "Error: VERSION file missing. Unable to proceed."
+    exit 1
+fi
+VERSION=`cat VERSION`
+TAG=${MERCURE_TAG:-$VERSION}
+
 # Define where mercure is going to store things
 # You can redefine types of volumes in docker/docker-compose.yml
 MERCUREBASE=/opt/mercure
@@ -30,7 +40,18 @@ MERCURESRC=./
 #################################################################
 # BUILD SECTION
 #################################################################
-docker build -t $PREFIX/mercure-base:latest -f docker/base/Dockerfile .
+echo "Building Docker containers for mercure $VERSION"
+echo "Using image tag $TAG"
+echo ""
+read -p "Proceed (y/n)? " ANS
+if [ "$ANS" = "y" ]; then
+echo ""
+else
+echo "Aborted."
+exit 0
+fi
+
+docker build -t $PREFIX/mercure-base:$TAG -f docker/base/Dockerfile .
 docker build docker/ui -t $PREFIX/mercure-ui:$TAG
 docker build docker/bookkeeper -t $PREFIX/mercure-bookkeeper:$TAG
 docker build docker/cleaner -t $PREFIX/mercure-cleaner:$TAG
@@ -38,3 +59,10 @@ docker build docker/dispatcher -t $PREFIX/mercure-dispatcher:$TAG
 docker build docker/processor -t $PREFIX/mercure-processor:$TAG
 docker build docker/receiver -t $PREFIX/mercure-receiver:$TAG
 docker build docker/router -t $PREFIX/mercure-router:$TAG
+docker build nomad/sshd -t $PREFIX/alpine-sshd:latest
+docker build nomad/processing -t $PREFIX/processing-step:$TAG
+docker build nomad/dummy-processor -t $PREFIX/mercure-dummy-processor:$TAG
+
+echo ""
+echo "Done."
+echo ""
--- push-docker.sh
+++ push-docker.sh
@@ -1,11 +1,36 @@
-docker push mercureimaging/mercure-base:latest
-docker push mercureimaging/mercure-router:latest
-docker push mercureimaging/mercure-processor:latest
-docker push mercureimaging/mercure-receiver:latest
-docker push mercureimaging/mercure-dispatcher:latest
-docker push mercureimaging/mercure-bookkeeper:latest
-docker push mercureimaging/mercure-cleaner:latest
-docker push mercureimaging/mercure-ui:latest
+# Read the version of the mercure source, which will be used for
+# tagging the Docker images, unless a tag has been provided 
+# through the environment variable MERCURE_TAG
+if [ ! -f "VERSION" ]; then
+    echo "Error: VERSION file missing. Unable to proceed."
+    exit 1
+fi
+VERSION=`cat VERSION`
+TAG=${MERCURE_TAG:-$VERSION}
+
+echo "Pushing Docker containers for mercure $VERSION"
+echo "Using image tag $TAG"
+echo ""
+read -p "Proceed (y/n)? " ANS
+if [ "$ANS" = "y" ]; then
+echo ""
+else
+echo "Aborted."
+exit 0
+fi
+
+docker push mercureimaging/mercure-base:$TAG
+docker push mercureimaging/mercure-router:$TAG
+docker push mercureimaging/mercure-processor:$TAG
+docker push mercureimaging/mercure-receiver:$TAG
+docker push mercureimaging/mercure-dispatcher:$TAG
+docker push mercureimaging/mercure-bookkeeper:$TAG
+docker push mercureimaging/mercure-cleaner:$TAG
+docker push mercureimaging/mercure-ui:$TAG
 docker push mercureimaging/alpine-sshd:latest
-docker push mercureimaging/processing-step:latest
-docker push mercureimaging/mercure-dummy-processor:latest
+docker push mercureimaging/processing-step:$TAG
+docker push mercureimaging/mercure-dummy-processor:$TAG
+
+echo ""
+echo "Done."
+echo ""
