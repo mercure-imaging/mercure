d4f0723cbecec17c21c33c4acff3b9a285d4fb76
Roy Wiggins <Roy.Wiggins@nyulangone.org>
2022-03-08 18:38:58 +0000

    minor docker build change

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo
+set -euo pipefail
 
 #################################################################
 # Create mercure from scratch in Docker environment
@@ -74,14 +74,18 @@ else
   fi
 fi
 
-docker build --no-cache -t $PREFIX/mercure-base:$TAG -f docker/base/Dockerfile .
-docker build docker/ui -t $PREFIX/mercure-ui:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/bookkeeper -t $PREFIX/mercure-bookkeeper:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/cleaner -t $PREFIX/mercure-cleaner:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/dispatcher -t $PREFIX/mercure-dispatcher:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/processor -t $PREFIX/mercure-processor:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/receiver -t $PREFIX/mercure-receiver:$TAG --build-arg VERSION_TAG=$TAG
-docker build docker/router -t $PREFIX/mercure-router:$TAG --build-arg VERSION_TAG=$TAG
+build_component () {
+  docker build docker/$1 -t $PREFIX/mercure-$1:$TAG --build-arg VERSION_TAG=$TAG
+  docker tag $PREFIX/mercure-$1:$TAG $PREFIX/mercure-$1:latest
+}
+
+docker build -t $PREFIX/mercure-base:$TAG -f docker/base/Dockerfile .
+
+for component in ui bookkeeper cleaner processor receiver router
+do
+  build_component $component
+done
+
 docker build nomad/sshd -t $PREFIX/alpine-sshd:latest
 docker build nomad/processing -t $PREFIX/processing-step:$TAG
 docker build nomad/dummy-processor -t $PREFIX/mercure-dummy-processor:$TAG
