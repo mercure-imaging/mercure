98c2992251ceddd8399a030c8a5a9080ad3c046b
Tobias Block <tobias.block@nyumc.org>
2022-12-19 13:05:44 +0000

    Added protection against concurrent process

--- process/status.py
+++ process/status.py
@@ -13,10 +13,15 @@ from common.constants import mercure_names
 
 def is_ready_for_processing(folder) -> bool:
     """Checks if a case in the processing folder is ready for the processor."""
-    path = Path(folder)
-    folder_status = (
-        not (path / mercure_names.LOCK).exists()
-        and not (path / mercure_names.PROCESSING).exists()
-        and len(list(path.glob("*.dcm"))) > 0
-    )
-    return folder_status
+    try:
+        path = Path(folder)
+        folder_status = (
+            not (path / mercure_names.LOCK).exists()
+            and not (path / mercure_names.PROCESSING).exists()
+            and len(list(path.glob("*.dcm"))) > 0
+        )
+        return folder_status
+    except:
+        # Capture exceptions that may be triggered if the folder has been removed 
+        # by another process in the meantime
+        return False
