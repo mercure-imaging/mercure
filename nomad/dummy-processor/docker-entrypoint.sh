620a86ce8be6f40fbc1ba871c5ca364fcb0bf8df
Roy Wiggins <Roy.Wiggins@nyulangone.org>
2023-06-23 15:28:12 +0000

    initial work

--- nomad/dummy-processor/docker-entrypoint.sh
+++ nomad/dummy-processor/docker-entrypoint.sh
@@ -9,4 +9,5 @@ cp -r $MERCURE_IN_DIR/. $MERCURE_OUT_DIR
 echo "Output:"
 ls -al $MERCURE_OUT_DIR
 
+echo '{"foo":"bar"}' > "$MERCURE_OUT_DIR/result.json"
 echo "Processed."
--- webgui.py
+++ webgui.py
@@ -517,7 +517,7 @@ async def self_test(request) -> Response:
         )
         if test_type == "process":
             config.mercure.modules[test_rule + "_self_test_module"] = Module(
-                docker_tag="mercureimaging/mercure-dummy-processor:0.2.0-beta.7",
+                docker_tag=f"mercureimaging/mercure-dummy-processor:{mercure_defs.VERSION}",
             )
             config.mercure.modules[test_rule + "_self_test_module_2"] = Module(
                 docker_tag="mercureimaging/mercure-dummy-processor:0.2.0-beta.7",
