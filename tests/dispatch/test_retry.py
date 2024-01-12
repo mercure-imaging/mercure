ecbc4b81ac6a2534c915471d86fdeff4db289d85
Tobias Block <tobias.block@nyumc.org>
2024-01-12 05:49:27 +0000

    Fix for test

--- tests/dispatch/test_retry.py
+++ tests/dispatch/test_retry.py
@@ -17,6 +17,7 @@ dummy_info = {
     "triggered_rules": "",
     "mrn": "",
     "acc": "",
+    "sender_address": "localhost",
     "mercure_version": "",
     "mercure_appliance": "",
     "mercure_server": "",
