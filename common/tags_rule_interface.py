c77949ef81000622c4d37bdabf954b81af5d8080
Roy Wiggins <Roy.Wiggins@nyulangone.org>
2024-06-27 01:21:34 +0000

    when evaluating rules, keep track of accessed tags

--- common/tags_rule_interface.py
+++ common/tags_rule_interface.py
@@ -1,31 +1,44 @@
 class Tags:
+    _tags_dict = None
+    _tags_accessed = None
+
     def __init__(self, input_dict) -> None:
-        self._dict = input_dict
+        self._tags_dict = input_dict
+        self._tags_accessed = set()
+
+    def tags_accessed(self):
+        return self._tags_accessed
 
     def __getattr__(self, name):
-        if name in self._dict:
-            return self._dict[name]
+        if name in self.__dict__:
+            return self.__dict__[name]
+
+        if name in self._tags_dict:
+            self._tags_accessed.add(name)
+            return self._tags_dict[name]
         else:
             lowered_name = name.lower()
-            for k in self._dict:
+            for k in self._tags_dict:
                 if k.lower() == lowered_name:
-                    return self._dict[k]
+                    self._tags_accessed.add(k)
+                    return self._tags_dict[k]
             raise TagNotFoundException(f"No such tag '{name}' in tags list.")
 
     def __getitem__(self, name):
-        if name in self._dict:
-            return self._dict[name]
+        if name in self._tags_dict:
+            self._tags_accessed.add(name)
+            return self._tags_dict[name]
         else:
             raise TagNotFoundException(f"No such tag '{name}' in tags list.")
 
     def __len__(self):
-        return len(self._dict)
+        return len(self._tags_dict)
 
     def __iter__(self):
-        return iter(self._dict)
+        return iter(self._tags_dict)
 
     def __contains__(self, name):
-        return name in self._dict
+        return name in self._tags_dict
 
 class TagNotFoundException(Exception):
     pass
