13741b07a215a6a95a479a3f206d07c1bbee33c1
Tobias Block <tobias.block@nyumc.org>
2024-11-01 00:00:14 +0000

    Fix for install script.

--- install.sh
+++ install.sh
@@ -357,6 +357,11 @@ install_packages() {
   echo "## Installing Linux packages..."
   sudo apt-get update
   sudo apt-get install -y build-essential wget git dcmtk jq inetutils-ping sshpass rsync postgresql postgresql-contrib libpq-dev git-lfs python3-wheel python3-dev python3 python3-venv sendmail libqt5core5a redis
+  if [ $UBUNTU_VERSION == "24.04" ]; then
+    sudo apt-get install -y libqt6core6t64 
+  else
+    sudo apt-get install -y libqt5core5a 
+  fi
 }
 
 install_dependencies() {
